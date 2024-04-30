f7eab30dffdec4addc3c1f187c737ce4af757f34
junghongseop <a01082372487@gmail.com>
2024-04-30 17:52:13 +0900

    ADD :: 기본 메인페이지

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import MainScreen from './components/MainScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppStackNavigator = createAppContainer ({
+    Main: {
+        screen: MainScreen
+    }
 });
+
+export default createAppContainer (AppStackNavigator);
